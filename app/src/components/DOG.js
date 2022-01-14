42b4299ad6acbf7999360d022d5c0bd8c95c62dc
Jesse <eoshout@Gmail.com>
2022-01-13 21:49:23 -0500

    Project Async

--- app/src/components/DOG.js
+++ app/src/components/DOG.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react';
-import { getDog, fetchFail } from '../actions';
-import { connect } from 'react-redux';
-import "../App.css"
+import React, { useEffect } from "react";
+import { getDog, fetchFail } from "../actions";
+import { connect } from "react-redux";
+import "../App.css";
 
 const Dog = (props) => {
   const { isFetching, error } = props;
-  const name = props.name
-  useEffect(()=> {
+  const name = props.name;
+  useEffect(() => {
     props.getDog();
   }, []);
 
@@ -18,30 +18,38 @@ const Dog = (props) => {
     return <h2>Fetching dog for ya!</h2>;
   }
 
-  const handleClick = ()=> {
+  const handleClick = () => {
     props.getDog();
-  }
+  };
 
   return (
-    <><div className="ClassyDog">
-      <div>
-      </div>
-      <button className="goodboy" onClick={handleClick}>Get new dog</button>
-      <button className="goodboy" onClick={()=> {
-        props.fetchFail("Pressed the Error button!!!");
-      }}> Error Button</button>
-      <h2>heres the dog you asked for</h2>
-      <img className="rotate" src={name}/>
+    <>
+      <div className="ClassyDog">
+        <div></div>
+        <button className="goodboy" onClick={handleClick}>
+          Get new dog
+        </button>
+        <button
+          className="goodboy"
+          onClick={() => {
+            props.fetchFail("Pressed the Error button!!!");
+          }}
+        >
+          {" "}
+          Error Button
+        </button>
+        <h2>heres the dog you asked for</h2>
+        <img className="rotate" src={name} />
       </div>
     </>
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     name: state.name,
     isFetching: state.isFetching,
-    error: state.error
+    error: state.error,
   };
 };
 
